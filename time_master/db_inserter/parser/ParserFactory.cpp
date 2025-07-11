7dbf46e9ad7fa0c0012da8d0a37c433c3ec92c94
camellia2077 <130271834+camellia2077@users.noreply.github.com>
2025-07-11 21:54:38 +0800

    优化parser结构

--- time_master/db_inserter/parser/ParserFactory.cpp
+++ time_master/db_inserter/parser/ParserFactory.cpp
@@ -1,6 +1,6 @@
 #include "ParserFactory.h"
-#include "ConfigLoader.h" // 工厂内部依赖ConfigLoader来加载配置
-#include "ParserConfig.h" // 工厂内部需要知道配置的数据结构
+#include "parser/internal/ConfigLoader.h" // 工厂内部依赖ConfigLoader来加载配置
+#include "parser/internal/ParserConfig.h" // 工厂内部需要知道配置的数据结构
 
 /**
  * @brief 实现创建解析器的具体逻辑。
