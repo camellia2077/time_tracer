5b508714316f884b8f0b36aed60702cc6e4a00e5
camellia <130271834+camellia2077@users.noreply.github.com>
2025-07-21 20:29:21 +0800

    格式化返回一个更通用的内容tex

--- time_master/queries/report_generators/daily/formatter/day_tex/DailyTex.h
+++ time_master/queries/report_generators/daily/formatter/day_tex/DailyTex.h
@@ -1,3 +1,4 @@
+// DailyTex.h
 #ifndef DAILY_REPORT_TEX_FORMATTER_H
 #define DAILY_REPORT_TEX_FORMATTER_H
 
@@ -7,6 +8,7 @@
 /**
  * @class DailyTex
  * @brief 将日报数据格式化为 TeX 字符串的具体实现。
+ * (现在使用 ProjectBreakdownFormatterFactory 来处理项目明细)
  */
 class DailyTex : public IReportFormatter {
 public:
@@ -18,7 +20,7 @@ private:
     void _display_preamble(std::stringstream& ss) const;
     void _display_header(std::stringstream& ss, const DailyReportData& data) const;
     void _display_project_breakdown(std::stringstream& ss, const DailyReportData& data, sqlite3* db) const;
-    std::string _escape_tex(const std::string& s) const;
+    // _escape_tex 函数声明已被移除
 };
 
 #endif // DAILY_REPORT_TEX_FORMATTER_H
