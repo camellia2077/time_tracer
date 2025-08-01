faf6af0305448c3d3e5648030bbd0379b0e80a76
camellia <130271834+camellia2077@users.noreply.github.com>
2025-08-01 13:05:30 +0800

    接口写成模板

--- time_master/queries/period/PeriodFmtFactory.cpp
+++ time_master/queries/period/PeriodFmtFactory.cpp
@@ -1,18 +1,17 @@
+// PeriodFmtFactory.cpp
 #include "PeriodFmtFactory.h"
-#include "queries/period/formatters/PeriodMd.h" // Corrected header
+#include "queries/period/formatters/PeriodMd.h"
 #include "queries/period/formatters/PeriodTex.h"
 
 #include <stdexcept>
 
-// create_formatter 方法的实现
-std::unique_ptr<IPeriodFmt> PeriodFmtFactory::create_formatter(ReportFormat format) {
+// create_formatter 方法的实现，返回类型已更新
+std::unique_ptr<IReportFormatter<PeriodReportData>> PeriodFmtFactory::create_formatter(ReportFormat format) {
     switch (format) {
         case ReportFormat::Markdown:
             return std::make_unique<PeriodMd>();
         case ReportFormat::LaTeX:
             return std::make_unique<PeriodTex>();
-        // case ReportFormat::Json:
-        //     return std::make_unique<PeriodReportJsonFormatter>();
         default:
             throw std::invalid_argument("Unsupported report format requested for period report.");
     }
