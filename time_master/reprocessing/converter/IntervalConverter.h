3a7c374194d1163de42d2decf2596f10b973edcc
camellia <130271834+camellia2077@users.noreply.github.com>
2025-08-17 00:23:20 +0800

    优化IntervalConverter结构

--- time_master/reprocessing/converter/IntervalConverter.h
+++ time_master/reprocessing/converter/IntervalConverter.h
@@ -1,10 +1,12 @@
-// reprocessing/input_transfer/IntervalConverter.h
+// reprocessing/converter/IntervalConverter.h
 
 #ifndef INTERVAL_PROCESSOR_H
 #define INTERVAL_PROCESSOR_H
 
 #include <string>
-#include "reprocessing/converter/internal/ConverterConfig.h" // 包含配置类
+#include <unordered_set>
+#include "reprocessing/converter/internal/ConverterConfig.h"
+#include "reprocessing/converter/model/InputData.h"
 
 class IntervalConverter {
 public:
@@ -13,7 +15,13 @@ public:
     bool executeConversion(const std::string& input_filepath, const std::string& output_filepath, const std::string& year_prefix);
 
 private:
-    ConverterConfig config_; // 将配置作为成员变量
+    // 将配置和关键词设为成员变量
+    ConverterConfig config_;
+    const std::unordered_set<std::string> wake_keywords_;
+
+    // 私有辅助方法，用于封装逻辑
+    bool isNewDayMarker(const std::string& line) const;
+    void parseLine(const std::string& line, InputData& currentDay) const;
 };
 
 #endif // INTERVAL_PROCESSOR_H
