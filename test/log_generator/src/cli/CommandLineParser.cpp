f458a67f965c397d46ded0d968ec6877e282da1e
camellia <130271834+camellia2077@users.noreply.github.com>
2025-09-08 14:53:33 +0800

    颜色写成宏定义

--- test/log_generator/src/cli/CommandLineParser.cpp
+++ test/log_generator/src/cli/CommandLineParser.cpp
@@ -1,10 +1,11 @@
 #include "cli/CommandLineParser.h"
-#include "utils/Utils.h"
-#include "version.h" // [核心修改] 引入新的版本头文件
+#include "common/AnsiColors.hpp" // [核心修改] 引入新的颜色头文件
+#include "version.h"
 #include <iostream>
 #include <string>
 #include <stdexcept>
 
+// ... 构造函数和 version_requested/print_version 不变 ...
 CommandLineParser::CommandLineParser(int argc, char* argv[])
     : argc_(argc), argv_(argv), prog_name_(argv[0]) {}
 
@@ -13,13 +14,14 @@ bool CommandLineParser::version_requested() const {
 }
 
 void CommandLineParser::print_version() const {
-    // [核心修改] 使用从 version.h 中引入的常量
     std::cout << "log_generator version " << AppVersion::APP_VERSION << std::endl;
     std::cout << "Last Updated: " << AppVersion::LAST_UPDATE << std::endl;
 }
 
+
 void CommandLineParser::print_usage() const {
-    std::cerr << Utils::ConsoleColors::red << "Usage: " << Utils::ConsoleColors::reset << prog_name_ << " <start_year> <end_year> <items_per_day>\n";
+    // [核心修改] 使用新的颜色宏
+    std::cerr << RED_COLOR << "Usage: " << RESET_COLOR << prog_name_ << " <start_year> <end_year> <items_per_day>\n";
     std::cerr << "       " << prog_name_ << " --version\n";
     std::cerr << "Description: Generates test log data for a given year range. Reads activities from 'activities_config.json'.\n";
     std::cerr << "  <start_year>      : The starting year (e.g., 1990).\n";
@@ -42,31 +44,36 @@ std::optional<Config> CommandLineParser::parse() {
         config.items_per_day = std::stoi(argv_[3]);
     }
     catch (const std::invalid_argument&) {
-        std::cerr << Utils::ConsoleColors::red << "Error" << Utils::ConsoleColors::reset << ": Invalid argument. All arguments must be integers.\n";
+        // [核心修改] 使用新的颜色宏
+        std::cerr << RED_COLOR << "Error" << RESET_COLOR << ": Invalid argument. All arguments must be integers.\n";
         print_usage();
         return std::nullopt;
     }
     catch (const std::out_of_range&) {
-        std::cerr << Utils::ConsoleColors::red << "Error" << Utils::ConsoleColors::reset <<": Argument out of range.\n";
+        // [核心修改] 使用新的颜色宏
+        std::cerr << RED_COLOR << "Error" << RESET_COLOR <<": Argument out of range.\n";
         print_usage();
         return std::nullopt;
     }
 
     if (config.start_year <= 0 || config.end_year <= 0) {
-        std::cerr <<  Utils::ConsoleColors::red << "Error" << Utils::ConsoleColors::reset <<": Years must be positive integers.\n";
+        // [核心修改] 使用新的颜色宏
+        std::cerr <<  RED_COLOR << "Error" << RESET_COLOR <<": Years must be positive integers.\n";
         print_usage();
         return std::nullopt;
     }
     
     if (config.items_per_day < 2) {
-        std::cerr <<Utils::ConsoleColors::red << "Error" << Utils::ConsoleColors::reset <<": <items_per_day> must be 2 or greater to generate realistic sleep data.\n";
+        // [核心修改] 使用新的颜色宏
+        std::cerr << RED_COLOR << "Error" << RESET_COLOR <<": <items_per_day> must be 2 or greater to generate realistic sleep data.\n";
         std::cerr << "       (A value of 1 would make 'getup' the last event of the day).\n";
         print_usage();
         return std::nullopt;
     }
 
     if (config.end_year < config.start_year) {
-        std::cerr << Utils::ConsoleColors::red << "Error" << Utils::ConsoleColors::reset << ": <end_year> cannot be earlier than <start_year>.\n";
+        // [核心修改] 使用新的颜色宏
+        std::cerr << RED_COLOR << "Error" << RESET_COLOR << ": <end_year> cannot be earlier than <start_year>.\n";
         print_usage();
         return std::nullopt;
     }
