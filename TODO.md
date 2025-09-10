fb3a1c412d06961a8312b74156973ac3b461b091
camellia <130271834+camellia2077@users.noreply.github.com>
2025-09-10 14:04:38 +0800

    格式化输出，根据类型输出不同的颜色。

--- TODO.md
+++ TODO.md
@@ -1,10 +1,8 @@
 ## 格式化输出，根据类型输出不同的颜色。
-
+typst已经实现
 
 
 ## 修改测试程序，增加通宵的模拟，
-
-
 
 ## InputParser.cpp中引用了 #include "reprocessing/validator/common/ValidatorUtils.hpp" 需要解耦
 
--- time_master/src/queries/daily/formatters/typ/DayTyp.hpp
+++ time_master/src/queries/daily/formatters/typ/DayTyp.hpp
@@ -21,6 +21,13 @@ private:
     void _display_project_breakdown(std::stringstream& ss, const DailyReportData& data, sqlite3* db) const;
     void _display_statistics(std::stringstream& ss, const DailyReportData& data) const;
     void _display_detailed_activities(std::stringstream& ss, const DailyReportData& data) const;
+
+    /**
+     * @brief 格式化单行活动记录，为特定关键词应用颜色。
+     * @param record 单个时间记录。
+     * @return 格式化后的 Typst 字符串。
+     */
+    std::string _format_activity_line(const TimeRecord& record) const;
 };
 
 #endif // DAILY_REPORT_TYP_FORMATTER_H
