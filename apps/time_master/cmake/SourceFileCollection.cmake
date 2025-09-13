3ddd9207969514ed6d709f369983298dd846e528
camellia <130271834+camellia2077@users.noreply.github.com>
2025-09-13 14:45:40 +0800

    更改文件夹命名

--- apps/time_master/cmake/SourceFileCollection.cmake
+++ apps/time_master/cmake/SourceFileCollection.cmake
@@ -89,13 +89,15 @@ set(REPROCESSING_SOURCES
 
     "src/reprocessing/validator/common/ValidatorUtils.cpp"
 
-    # 验证封装
+    # 验证json的封装
     "src/reprocessing/validator/output_json/facade/JsonValidator.cpp"
     # 验证实现
-    "src/reprocessing/validator/output_json/_internal/JsonValidatorActivities.cpp"
-    "src/reprocessing/validator/output_json/_internal/JsonValidatorDate.cpp"
-    "src/reprocessing/validator/output_json/_internal/JsonValidatorRules.cpp"
-    "src/reprocessing/validator/output_json/_internal/JsonValidatorTime.cpp"
+    "src/reprocessing/validator/output_json/pipelines/JsonValidatorActivities.cpp"
+    "src/reprocessing/validator/output_json/pipelines/JsonValidatorDate.cpp"
+    "src/reprocessing/validator/output_json/pipelines/JsonValidatorRules.cpp"
+    "src/reprocessing/validator/output_json/pipelines/JsonValidatorTime.cpp"
+
+
 
     "src/reprocessing/validator/source_txt/SourceFileValidator.cpp"
 
--- apps/time_master/src/reprocessing/validator/output_json/facade/JsonValidator.hpp
+++ apps/time_master/src/reprocessing/validator/output_json/facade/JsonValidator.hpp
@@ -7,10 +7,10 @@
 #include <set>
 #include <nlohmann/json.hpp>
 
-#include "reprocessing/validator/output_json/_internal/JsonValidatorDate.hpp"
-#include "reprocessing/validator/output_json/_internal/JsonValidatorTime.hpp"
-#include "reprocessing/validator/output_json/_internal/JsonValidatorRules.hpp"
-#include "reprocessing/validator/output_json/_internal/JsonValidatorActivities.hpp"
+#include "reprocessing/validator/output_json/pipelines/JsonValidatorDate.hpp"
+#include "reprocessing/validator/output_json/pipelines/JsonValidatorTime.hpp"
+#include "reprocessing/validator/output_json/pipelines/JsonValidatorRules.hpp"
+#include "reprocessing/validator/output_json/pipelines/JsonValidatorActivities.hpp"
 
 // 这个模块专门负责验证经过转换后生成的 JSON 输出文件 的数据结构和内容
 /**
