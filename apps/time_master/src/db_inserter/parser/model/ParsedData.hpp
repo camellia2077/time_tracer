a0b1275a620ba857cf89029d4022ff00a12f8502
camellia <130271834+camellia2077@users.noreply.github.com>
2025-09-13 16:09:24 +0800

    优化db_inserter的parser

---
+++ apps/time_master/src/db_inserter/parser/model/ParsedData.hpp
@@ -0,0 +1,16 @@
+// db_inserter/parser/model/ParsedData.hpp
+#ifndef PARSED_DATA_HPP
+#define PARSED_DATA_HPP
+
+#include "db_inserter/model/time_sheet_model.hpp"
+#include <vector>
+#include <unordered_set>
+
+// 统一存放所有解析出的数据
+struct ParsedData {
+    std::vector<DayData> days;
+    std::vector<TimeRecordInternal> records;
+    std::unordered_set<std::pair<std::string, std::string>, pair_hash> parent_child_pairs;
+};
+
+#endif // PARSED_DATA_HPP
